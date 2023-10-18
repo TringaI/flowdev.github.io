03f6d175c22e9fb7ee3210a80c83c95bb312a602
Tringa <tringaibrahimii@gmail.com>
2023-10-18 20:29:43 +0200

    window

--- pages/Components/Services/Components/Cards.jsx
+++ pages/Components/Services/Components/Cards.jsx
@@ -13,7 +13,24 @@ function Cards(props) {
     const handleMouseLeave = () => {
         setIsMouseOver(false);
     }
+    const [windowWidth, setWindowWidth] = useState(0);
 
+    useEffect(() => {
+      // Update window width when the component is mounted
+      setWindowWidth(window.innerWidth);
+  
+      // Add an event listener to update window width when the window is resized
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      // Remove the event listener when the component is unmounted
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
     return (
         <div
             className={`${style.services_content_container} ${isMouseOver ? `${style.afterHover}` :`${style.beforeHover}`} relative z-40`}
